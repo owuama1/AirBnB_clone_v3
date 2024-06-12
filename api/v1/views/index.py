58ffac8044ccfada919b1c93dc60ea5dc3946089
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 17:47:24 +0000

    update index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3xx
 '''api status'''
-# api/v1/views/index.py
-
 from flask import jsonify
-from api.v1.views import app_views
-
+from models import storage
+from . import app_views
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 @app_views.route("/status", methods=["GET"])
 def status():
     """Return status OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    """Return statistics about the number of each object type"""
+    stats = {
+        "states": storage.count(State),
+        "users": storage.count(User),
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review)
+    }
+    return jsonify(stats)
