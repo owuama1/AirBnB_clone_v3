8ca330978ac0df66e7c0ec01d2c24c6b874d5796
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 17:24:41 +0000

    make some changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""Flask server (variable app)"""
+"""Flask server (variable app)
+"""
 
-
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
-from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    '''Closes the storage'''
+    """Closes the storage"""
     storage.close()
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    '''Return JSON response for 404 error'''
-    return jsonify({"error": "Not found"}), 404
-
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
