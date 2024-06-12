f36cb3d9a045a138bc2b6d2a240c15a9f8673552
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 17:37:17 +0000

    make executable

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,3 @@
-# api/v1/views/__init__.py
-
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route("/status", methods=["GET"])
 def status():
     """Return status OK"""
