535acadee1699fda97e51f28cb5382a1c7b98708
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 17:21:24 +0000

    make some changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
-"""Flask server (variable app)
-"""
+"""Flask server (variable app)"""
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """Closes the storage"""
+    '''Closes the storage'''
     storage.close()
 
 
-app.register_blueprint(app_views, url_prefix="/api/v1")
+@app.errorhandler(404)
+def page_not_found(error):
+    '''Return JSON response for 404 error'''
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 '''api status'''
-
 from flask import jsonify
-from . import app_views
+from api.v1.views import app_views
+from models import storage, State, User, Amenity, City, Place, Review
 
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
 
-@app_views.route("/status", methods=["GET"])
-def status():
-    """Return status OK"""
-    return jsonify({"status": "OK"})
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stuff():
+    '''JSON Responses'''
+    todos = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
