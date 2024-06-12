d1fd9206a24f33e7712c2c04e9e0e178616b7c40
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 17:51:47 +0000

    create a handler for 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """Flask server (variable app)
 """
-
-from flask import Flask
-from models import storage
+from flask import Flask, jsonify
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """Closes the storage"""
+    '''Closes the storage'''
     storage.close()
 
 
-app.register_blueprint(app_views, url_prefix="/api/v1")
+@app.errorhandler(404)
+def page_not_found(error):
+    '''Return JSON response for 404 error'''
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
